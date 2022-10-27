470130570491760391ab03e40db9e350a93ab0b1
Phan Minh Nhựt <phanminhnhut.114@gmail.com>
2022-10-27 15:53:24 +0700

    add jwt authorize

--- src/utils/plugin/axios.js
+++ src/utils/plugin/axios.js
@@ -6,20 +6,20 @@ const instances = axios.create({
 })
 
 
-// instances.interceptors.request.use(
-//     config => {
-//         // ** Get token from localStorage
-//         const accessToken = localStorage.getItem('accessToken')
+instances.interceptors.request.use(
+    config => {
+        // ** Get token from localStorage
+        const accessToken = localStorage.getItem('accessToken')
 
-//         // ** If token is present add it to request's Authorization Header
-//         if (accessToken) {
-//             // ** eslint-disable-next-line no-param-reassign
-//             config.headers.Authorization = `Bearer ${accessToken}`
-//         }
-//         return config
-//     },
-//     error => Promise.reject(error)
-// )
+        // ** If token is present add it to request's Authorization Header
+        if (accessToken) {
+            // ** eslint-disable-next-line no-param-reassign
+            config.headers.Authorization = `Bearer ${accessToken}`
+        }
+        return config
+    },
+    error => Promise.reject(error)
+)
 
 // // ** Add request/response interceptor
 // instances.interceptors.response.use(
